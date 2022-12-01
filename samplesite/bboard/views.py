99e0c7e466992678fc342e821b82ed1d3707d574
t0lis <107796319+t0lis@users.noreply.github.com>
2022-12-01 16:51:19 +0700

    Add files via upload

---
+++ samplesite/bboard/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from .models import Bb
+from .models import Rubric
+
+
+def index(request):
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, './index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, './by_rubric.html', context)
